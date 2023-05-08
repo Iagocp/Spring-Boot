7d63c8bca485654409ee9a1488c7d8f20ef70207
Iagocp <114144454+Iagocp@users.noreply.github.com>
2023-05-08 10:07:35 +0200

    Actualización

---
+++ RickandMorty/src/main/java/com/cebem/RickandMorty/models/GeneratedValue.java
@@ -0,0 +1,5 @@
+package com.cebem.RickandMorty.models;
+
+public @interface GeneratedValue {
+
+}
---
+++ RickandMorty/src/main/java/com/cebem/RickandMorty/models/Info.java
@@ -0,0 +1,12 @@
+package com.cebem.RickandMorty.models;
+
+public class Info {
+    private int count;
+    public int  getCount(){
+        return count;
+    }
+    
+ 
+    
+    
+}
